7969b1b6374f3853a0c9ed988d3b28d687f377cd
Swaraj Roy <contactswarajroy@gmail.com>
2022-03-22 19:53:08 +0000

    Initial commit

---
+++ src/main/java/org/williamhill/service/scoreboard/ScoreboardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.williamhill.service.scoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreboardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/williamhill/service/scoreboard/ApplicationStartUpTest.java
@@ -0,0 +1,15 @@
+package org.williamhill.service.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
+class ApplicationStartUpTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
